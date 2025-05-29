55bb553f7aafbeff58f6e363bb8563bfa5a87650
ustalol <david.vut@mail.ru>
2025-05-29 22:48:30 +0300

    c

--- solver_lib/solver.cpp
+++ solver_lib/solver.cpp
@@ -8,7 +8,7 @@ void solve(float a, float b, float c, float& x1, float& x2)
 
     if (d < 0)
     {
-        throw std::logic_error{"error: discriminant < 0"};
+        throw std::logic_error("error: discriminant < 0");
     }
 
     x1 = (-b - sqrtf(d)) / (2 * a);
